69bb27b184f8258e373e4ad64714aa5083be2be4
terence.yoo <terence.yoo@kakaocorp.com>
2015-12-21 14:58:07 +0900

    add updateVersion.sh

---
+++ updateVersion.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+if [ $# -ne 1 ]; then
+  echo new version is required
+fi
+sed -i '' "s/\(.*<version.unified>\).*\(<\/version.unified>\)/\1$1\2/g" hbase0.94/pom.xml hbase0.96/pom.xml hbase0.98/pom.xml hbase1.0/pom.xml
